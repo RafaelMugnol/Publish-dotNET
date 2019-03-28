0154777deec09fb91bf8c7e3fed72989a0b149d6
Rafael Pedro Mugnol <rafael.mugnol@mercanet.local>
2019-03-28 15:42:27 -0300

    Melhoria na chamadas de funções com async/await.

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ const get = require("./actions/get");
 const publish = require("./actions/publish");
 const delivery = require("./actions/delivery");
 
-//inicia
-console.log("Iniciando...");
-alert("Tranferência iniciada.");
+async function main(){
+	//inicia
+	console.log("Iniciando...");
+	alert("Tranferência iniciada.");
 
-//questiona a versão 
-const version  = question();
-const fullVersion = getFullVersion(version);
+	//questiona a versão 
+	const version  = question();
+	const fullVersion = getFullVersion(version);
 
-//get no tfs
-get(fullVersion, () => {
+	//get no tfs
+	await convertToAsync(get, fullVersion);
 
 	//compila e publica localmente
-	publish(fullVersion, () => {
-		
-		//apaga web.config, transfere para o public e apaga a local
-		delivery(version, fullVersion, () => { alert("Tranferência finalizada!"); });
-		
-		console.log("Finalizado total! \nVersão: " + version);
-		
-	});
-});
+	await convertToAsync(publish, fullVersion);
 
+	//apaga web.config, transfere para o public e apaga a local
+	delivery(version, fullVersion, () => { alert("Tranferência finalizada!"); });
+	
+	console.log("Finalizado total! \nVersão: " + version);
+}
+
+main();
 
 
 function getFullVersion (versionBase){
@@ -33,4 +33,13 @@ function getFullVersion (versionBase){
 		versionBase += ".1";
 
 	return versionBase;
+}
+
+//https://stackoverflow.com/questions/22519784/how-do-i-convert-an-existing-callback-api-to-promises
+function convertToAsync(func, ...params){
+	return new Promise((resolve, reject) => {
+        func(...params, () => {
+			resolve();
+        });
+    });
 }
